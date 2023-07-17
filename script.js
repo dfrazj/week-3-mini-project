bc149823e0a039285121b1384d1ff410f5c4a5d6
Daniel Frazier <dfrazier.jl@gmail.com>
2023-07-16 18:18:13 -0700

    initial commit - game function and random computer choice

---
+++ script.js
@@ -0,0 +1,22 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var options = ["R", "P", "S"];
+
+function playGame() {
+    var userChoice = window.prompt("Enter R, P, or S:");
+
+    if(!userChoice) {
+        console.log('Game ended.');
+        return;
+    }
+
+    userChoice = userChoice.toUpperCase();
+    var computerChoice = options[Math.floor(Math.random() * options.length)];
+
+    window.alert("The computer chose " + computerChoice);
+}
+
+
+playGame();
